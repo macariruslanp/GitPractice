3b637b1771f32b041c4a54c856c4045908a96050
macari ruslan <macari.ruslan.p@gmail.com>
2019-02-26 12:39:13 +0200

    fixed new feature

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,18 @@
 
 public class Test {
 	public static void main(String[] args) {
+		if(args.length > 0 && args[0].equals("Test")) {
+			System.out.println("Test");
+			System.exit(0);
+		}
 		System.out.println("Hello Git");
 		System.out.println("new line");
+		newFeature();
 	}
 	
 	public static void newFeature() {
 		String arr[] = {"Test"};
-		Test test;
+		Test test = new Test();
 		test.main(arr);
 	}
 }

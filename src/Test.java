ab5779dcdfe776eee713d0a8dbf1f3e24c4ea5f1
macari ruslan <macari.ruslan.p@gmail.com>
2019-02-26 12:44:08 +0200

    changed newFeature

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("new line");
-		Test test;
+		Test test = new Test();
 		test.newFeature();
 	}
 	

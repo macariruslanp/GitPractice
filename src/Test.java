422f7b57aa1b9ee8d11978bc32e24034464f448b
macari ruslan <macari.ruslan.p@gmail.com>
2019-02-26 13:11:08 +0200

    added test new feature

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,16 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("new line");
+		test();
+		newFeature();
 	}
 	
 	public static void test() {
-		
+		System.out.println("test");
 	}
+	
+	public static void newFeature() {
+		System.out.println("new feature");
+	}
+	
 }
